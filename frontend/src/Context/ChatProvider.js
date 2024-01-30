a9cbce51158a35c3ebb2d8d1c968bd199f9800d5
Shubh Agrawal <shubhagrawal0710@gmail.com>
2024-01-30 05:47:44 +0530

    added

--- frontend/src/Context/ChatProvider.js
+++ frontend/src/Context/ChatProvider.js
@@ -6,14 +6,14 @@ const ChatContext = createContext();
 const ChatProvider = ({ children }) => {
   const [user, setUser] = useState();
   const history = useHistory();
-  useEffect(() => {
-    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-    setUser(userInfo);
+  // useEffect(() => {
+  //   const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  //   setUser(userInfo);
 
-    if (!userInfo) {
-      history.push("/");
-    }
-  }, [history]);
+  //   if (!userInfo) {
+  //     history.push("/");
+  //   }
+  // }, [history]);
 
   return (
     <ChatContext.Provider value={{ user, setUser }}>
--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,7 +1,45 @@
-import React from "react";
+import { Box, Button, Tooltip, Text, Menu, MenuButton } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const SideDrawer = () => {
-  return <div>Side Drawer</div>;
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState();
+
+  return (
+    <>
+      <Box
+        d="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="#F0F8FF"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
+        <Tooltip label="Search Users" hasArrow placement="bottom-end">
+          <Button variant="ghost">
+            <i class="fa fa-search" aria-hidden="true"></i>
+            <Text d={{ base: "none", md: "flex" }} px={"4"}>
+              Search Users
+            </Text>
+          </Button>
+        </Tooltip>
+        <Text
+          fontSize="2xl"
+          fontFamily={"cursive"}
+          float={"right"}
+          color={"#72A0C1"}
+        >
+          VAARTALAAP
+        </Text>
+        <Menu>
+          <MenuButton p={"10px"}></MenuButton>
+        </Menu>
+      </Box>
+    </>
+  );
 };
 
 export default SideDrawer;
